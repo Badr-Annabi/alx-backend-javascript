c4924d6748060cf71d976484b661347c2536e1c6
Badr-Annabi <badrannabi@gmail.com>
2024-05-29 13:15:37 +0100

    TASK 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    var task1 = true;
-    var task3 = false;
+    var task3 = true;
+    var task4 = false;
   }
 
   return [task, task2];
