767dc740ce97bcff278a7b1d1a19ac6479774cc0
Badr-Annabi <badrannabi@gmail.com>
2024-05-30 16:40:26 +0100

    TASK 3 display currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,16 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name}  (${this._code})`;
+  }
+}
