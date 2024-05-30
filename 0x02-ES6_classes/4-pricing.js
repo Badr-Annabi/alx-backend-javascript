232675123604f1af0242434896c11b378036343c
Badr-Annabi <badrannabi@gmail.com>
2024-05-31 00:20:29 +0100

    TASK 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,47 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(valueAmount) {
+    if (typeof valueAmount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = valueAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(valueCurrency) {
+    if (!(valueCurrency instanceof Currency)) {
+      throw new TypeError('currency must be a number');
+    }
+    this._currency = valueCurrency;
+  }
+
+  displayFulPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('both amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
