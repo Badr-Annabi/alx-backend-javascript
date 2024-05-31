7275601b2ac8aacf1ebf3ec8a97c1aaee44efd9d
Badr-Annabi <badrannabi@gmail.com>
2024-05-31 01:41:30 +0100

    TASK 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,13 +1,14 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (!(sqft instanceof Building) || (typeof sqft !== 'number')) {
-      throw new Error('sqft must be a number');
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
     if (typeof floors !== 'number') {
       throw new TypeError('floor must be a number');
     }
+    super(sqft);
     this._sqft = sqft;
     this._floors = floors;
   }
