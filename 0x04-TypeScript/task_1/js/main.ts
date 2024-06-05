967f0e577074dfb4f4e8da71464c39b320988090
Badr Annabi <badrannabi@Badrs-MacBook-Pro.local>
2024-06-05 17:07:56 +0100

    TASK 2 & 3

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,31 @@
+interface Teacher {
+    readonly firstName : string;
+    readonly lastName: string;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
