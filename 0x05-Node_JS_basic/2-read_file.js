86cdc00c24e577a0c3e8361a26866f6a628531a9
Badr_Annabi <badrannabi@gmail.com>
2024-07-23 23:55:43 +0100

    TASK 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const header = lines[0].split(',');
+
+    const students = {};
+    let totalStudents = 0;
+
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i].split(',');
+      if (line.length === header.length) {
+        const field = line[header.indexOf('field')];
+        const firstname = line[header.indexOf('firstname')];
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+        totalStudents++;
+      }
+    }
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const field in students) {
+      if (students.hasOwnProperty(field)) {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
