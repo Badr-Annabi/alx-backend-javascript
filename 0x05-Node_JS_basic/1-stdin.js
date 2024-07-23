fda72a21522506f46e9c9eba52f7880374270593
Badr_Annabi <badrannabi@gmail.com>
2024-07-23 17:02:40 +0100

    TASK 1 add trim

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
     process.exit();
   });
   process.on('exit', () => {
