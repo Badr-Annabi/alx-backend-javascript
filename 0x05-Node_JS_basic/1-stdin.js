dbacaf495f877a78239701ae9be17f0d5fade53e
Badr_Annabi <badrannabi@gmail.com>
2024-07-23 16:51:59 +0100

    TASK 1 work with process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.question('', (input) => {
-  console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing');
-  rl.close();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdin.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdin.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
