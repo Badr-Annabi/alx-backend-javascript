931c802cc23e67062657bcbe7c2b0d7f7882abd1
Badr_Annabi <badrannabi@gmail.com>
2024-07-24 01:03:47 +0100

    TASK 6 Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
-app.listen(port, () => {
-});
-
+app.listen(1245, '127.0.0.1');
 module.exports = app;
